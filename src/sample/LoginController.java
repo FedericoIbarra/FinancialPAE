eebfeae2cbc5c2d82c2f11332a2dfc2259e0aa7e
FedericoIbarra <is702981@iteso.mx>
2018-10-10 18:00:44 -0500

    Login View and Controller

---
+++ src/sample/LoginController.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML TextField tf_user;
+    @FXML TextField tf_password;
+
+    @FXML
+    public void initialize(){
+        //tf_password.getScene().getStylesheets().add("DarkTheme.css");
+
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Financial App");
+        Scene sc = new Scene(root, 800, 600);
+        sc.getStylesheets().add("sample/Styles/DarkTheme.css");
+        primaryStage.setScene(sc);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
