151c368e318c04a54f64f6317193c1105b509cef
FedericoIbarra <is702981@iteso.mx>
2018-10-11 09:47:11 -0500

    SignUp view and Controller

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -1,16 +1,31 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
 
 public class LoginController {
 
     @FXML TextField tf_user;
-    @FXML TextField tf_password;
+    @FXML PasswordField tf_password;
+    @FXML AnchorPane gp;
+
 
     @FXML
     public void initialize(){
         //tf_password.getScene().getStylesheets().add("DarkTheme.css");
 
+    }
+
+    public void signUp(ActionEvent actionEvent) throws IOException {
+        AnchorPane gp2 = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+        gp.getChildren().setAll(gp2);
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,13 +8,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public Parent root;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Financial App");
         Scene sc = new Scene(root, 800, 600);
         sc.getStylesheets().add("sample/Styles/DarkTheme.css");
         primaryStage.setScene(sc);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
