9b31b4c986d2ec9350da5049a8d53dc8a8ae727a
FedericoIbarra <is702981@iteso.mx>
2018-11-17 16:15:40 -0600

    Table improved

--- src/sample/Controllers/TableController.java
+++ src/sample/Controllers/TableController.java
@@ -2,24 +2,60 @@ package sample.Controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 import sample.DataModels.EntryTable;
+import sample.I18N;
 import sample.Session;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class TableController {
 
     private ObservableList<EntryTable> data = null;
 
+    private ResourceBundle currentRB;
+
     @FXML TableView table;
+    @FXML AnchorPane gp;
+    @FXML Label lb_title;
+    @FXML TableColumn col_reference;
+    @FXML TableColumn col_category;
+    @FXML TableColumn col_sub;
+    @FXML TableColumn col_amount;
+    @FXML TableColumn col_date;
 
     @FXML
     public void initialize(){
+
+        changeLanguage();
+
         data = Session.getSession().getData(true);
-
-        System.out.println(data.size());
-
         table.getItems().addAll(data);
 
+    }
+
+    private void changeLanguage() {
+
+        currentRB = I18N.getInstance().getResources();
+        lb_title.setText(currentRB.getString("entryTab"));
+        col_reference.setText(currentRB.getString("reference"));
+        col_category.setText(currentRB.getString("category"));
+        col_sub.setText(currentRB.getString("subcategory"));
+        col_amount.setText(currentRB.getString("amount"));
+        col_date.setText(currentRB.getString("date"));
+
+
+    }
+
+    public void goBack(ActionEvent actionEvent) throws IOException {
+        AnchorPane newAP = FXMLLoader.load(getClass().getResource("../Views/MainScreen.fxml"));
+        gp.getChildren().setAll(newAP);
     }
 }
--- src/sample/DataModels/EntryTable.java
+++ src/sample/DataModels/EntryTable.java
@@ -3,38 +3,77 @@ package sample.DataModels;
 import javafx.beans.property.SimpleFloatProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import sample.I18N;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ResourceBundle;
 
 public class EntryTable implements Serializable {
 
-    private final SimpleIntegerProperty category;
-    private final SimpleIntegerProperty subcategory;
+    private final SimpleStringProperty reference;
+    private final SimpleStringProperty category;
+    private final SimpleStringProperty subcategory;
     private final SimpleFloatProperty amount;
     private final SimpleStringProperty date;
 
+    private ResourceBundle currentRB;
+
     public EntryTable(Entry entry) {
 
+        currentRB = I18N.getInstance().getResources();
+
         this.reference = new SimpleStringProperty(entry.getReference());
-        this.category = new SimpleIntegerProperty(entry.getCategory());
-        this.subcategory = new SimpleIntegerProperty(entry.getSubcategory());
+
+        //Category
+
+        if(entry.getCategory() == 0) {
+            category = new SimpleStringProperty(currentRB.getString("act"));
+
+            if (entry.getSubcategory() == 0) subcategory = new SimpleStringProperty(currentRB.getString("cash"));
+            else if (entry.getSubcategory() == 1) subcategory = new SimpleStringProperty(currentRB.getString("tempinv"));
+            else if (entry.getSubcategory() == 2) subcategory = new SimpleStringProperty(currentRB.getString("ee"));
+            else if (entry.getSubcategory() == 3) subcategory = new SimpleStringProperty(currentRB.getString("inv"));
+            else if (entry.getSubcategory() == 4) subcategory = new SimpleStringProperty(currentRB.getString("other"));
+            else subcategory = null;
+
+        } else if (entry.getCategory() == 1) {
+            category = new SimpleStringProperty(currentRB.getString("pas"));
+
+            if (entry.getSubcategory() == 0) subcategory = new SimpleStringProperty(currentRB.getString("debtcp"));
+            else if (entry.getSubcategory() == 1) subcategory = new SimpleStringProperty(currentRB.getString("debtlp"));
+            else if (entry.getSubcategory() == 2) subcategory = new SimpleStringProperty(currentRB.getString("provider"));
+            else if (entry.getSubcategory() == 3) subcategory = new SimpleStringProperty(currentRB.getString("rpb"));
+            else if (entry.getSubcategory() == 4) subcategory = new SimpleStringProperty(currentRB.getString("other"));
+            else subcategory = null;
+
+        } else if (entry.getCategory() == 2) {
+            category = new SimpleStringProperty(currentRB.getString("patr"));
+
+            if (entry.getSubcategory() == 0) subcategory = new SimpleStringProperty(currentRB.getString("captsoc"));
+            else if (entry.getSubcategory() == 1) subcategory = new SimpleStringProperty(currentRB.getString("reserv"));
+            else if (entry.getSubcategory() == 2) subcategory = new SimpleStringProperty(currentRB.getString("util"));
+            else subcategory = null;
+        } else {
+            category = null;
+            subcategory = null;
+        }
+
         this.amount = new SimpleFloatProperty(entry.getAmount());
         this.date = new SimpleStringProperty(entry.getDate().toString());
 
     }
 
-    private final SimpleStringProperty reference;
 
     public SimpleStringProperty referenceProperty() {
         return reference;
     }
 
-    public SimpleIntegerProperty categoryProperty() {
+    public SimpleStringProperty categoryProperty() {
         return category;
     }
 
-    public SimpleIntegerProperty subcategoryProperty() {
+    public SimpleStringProperty subcategoryProperty() {
         return subcategory;
     }
 
